b7fcbc7af130e5ea46295e57521d9460132c85fd
Diwakargoswami <165192928+Diwakargoswami@users.noreply.github.com>
2024-11-11 10:27:21 +0530

    Add files via upload

---
+++ booklibrary.py
@@ -0,0 +1,10 @@
+bookDB = [
+    {'title': 'Godan', 'author': 'Preamchandra', 'ISBN': '978-3-16-148410-01', 'status': 'available', 'borrower': None},
+    {'title': 'Half Girlfriend', 'author': 'Chaten Bhagat', 'ISBN': '978-3-16-148410-02', 'status': 'available', 'borrower': None},
+    {'title': 'A Suitable Boy', 'author': 'Vikram Seth', 'ISBN': '978-3-16-148410-03', 'status': 'available',
+     'borrower': None},
+    {'title': 'Arthshastra', 'author': 'Kautilya', 'ISBN': '978-3-16-148410-04', 'status': 'available',
+     'borrower': None},
+    {'title': 'Gitanjali', 'author': 'Ravindranath Tagore', 'ISBN': '978-3-16-148410-05', 'status': 'available',
+     'borrower': None}
+]
